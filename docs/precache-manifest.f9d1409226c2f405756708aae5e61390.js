a83f8124925f183391631d148c3c3fd9fbff51c6
Karl Leswing <lilleswing@gmail.com>
2020-05-18 16:09:43 -0400

    Ready For publication

---
+++ docs/precache-manifest.f9d1409226c2f405756708aae5e61390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5ba2e761ae669be33513a346c860be",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "ce0249eaf5b16cbb847b",
+    "url": "/jeopardy/static/css/main.c0deef54.chunk.css"
+  },
+  {
+    "revision": "9d2cfe5526eb58360daf",
+    "url": "/jeopardy/static/js/2.a7ae52a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jeopardy/static/js/2.a7ae52a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0249eaf5b16cbb847b",
+    "url": "/jeopardy/static/js/main.857fa5df.chunk.js"
+  },
+  {
+    "revision": "2dd67a3c34aa670daee5",
+    "url": "/jeopardy/static/js/runtime-main.73963c4c.js"
+  }
+]);
